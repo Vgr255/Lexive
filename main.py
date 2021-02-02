97c1a0882c7a9dae5ac7a910f5a41e2dd99af1c7
Anilyka Barry <vgr255@live.ca>
2021-02-02 13:55:25 -0500

    Add Burrower

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ ctypes = {
     "O": "Xaxos: Outcast Ability", "C": "Curse",
     # Nemesis-specific types
     "N": "Corruption", "K": "Strike", "X": "Xaxos: Ascended Spell",
-    "B": "Bramble", "E": "Reminder",
+    "B": "Bramble", "T": "Trap", "E": "Reminder",
     # Minion types
     "MA": "Minion-Acolyte", "MP": "Minion-Pod", "MB": "Minion-Beacon",
     "MN": "Minion-Nemesis", "MC": "Minion-Claw", "MD": "Minion-Pylon",
