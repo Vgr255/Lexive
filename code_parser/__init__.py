baf9d3d5ff47e2a180aead8e2e7ad67a14d67962
Anilyka Barry <vgr255@live.ca>
2021-08-26 13:19:40 -0400

    Derp

--- code_parser/__init__.py
+++ code_parser/__init__.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from code_parser.player_cards import parse_player_card, format_player_card_effect, format_player_card_special
 
 def parse(code: str, type: str):
@@ -5,20 +7,21 @@ def parse(code: str, type: str):
         return parse_player_card(code)
     return code
 
-def format(code, type: str, name: str, ctype: str) -> str:
-    ret = ""
+def format(code, type: str, name: str, ctype: str) -> List[str]:
+    ret: List[str] = []
     if type == "PE":
-        ret = format_player_card_effect(code, name, ctype)
+        ret.append(format_player_card_effect(code, name, ctype))
     if type == "PS":
-        ret = format_player_card_special(code, name, ctype)
+        ret.extend(format_player_card_special(code, name, ctype))
     d = {}
     while True:
         try:
-            ret = ret.format(**d)
+            for i, r in enumerate(ret):
+                ret[i] = r.format(**d)
         except KeyError as e:
             d[e.args[0]] = f"ERROR: Item {e.args[0]!r} was not included in format string"
         except Exception as e:
-            ret = f"ERROR: Malformed code string {ret!r}"
+            ret[i] = f"ERROR: Malformed code string {r!r}"
             break
         else:
             break
